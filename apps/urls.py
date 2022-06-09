c5254e083ddfc5de8bcd5a673bda7bb2010c1efa
talesmm14 <talesmelquiades@hotmail.com>
2022-06-09 17:38:58 -0300

    add apps

---
+++ apps/urls.py
@@ -0,0 +1,7 @@
+# from django.urls import path, include
+#
+# urlpatterns = [
+#     path('auth/', include('apps.authentication.urls')),
+#     path('items/', include('apps.items.urls')),
+#     path('', include('apps.utils.urls')),
+# ]
